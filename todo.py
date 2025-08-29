0cc41a5b9e723850d9aba6fd9af6e978761a45d4
thomasbradley997 <thomas.e.bradley123@gmail.com>
2025-08-29 10:00:36 -0500

    added view tasks

---
+++ todo.py
@@ -0,0 +1,35 @@
+# todo.py
+
+# Step 1: Start with an empty list to hold tasks
+tasks = []
+
+# Step 2: Add a task
+def add_task(task):
+    tasks.append(task)
+
+# Step 3: View tasks
+def display_tasks(task):
+    for i, task in enumerate(tasks, start = 1)
+    print(f"{i}.{task}")
+
+# Step 4: Delete a task
+def delete_task(task):
+    tasks.pop(task)
+
+
+# Step 5: Mark task complete
+mark_complete = 0
+if task==complete:
+    mark_complete=mark_complete + 1
+
+# Step 6: Save/load tasks (extra stretch for today)
+
+
+# Demo flow (you can run this file directly: python todo.py)
+if __name__ == "__main__":
+    add_task("Finish Cyber 201 assignment")
+    add_task("Push code to GitHub")
+    view_tasks()
+    mark_complete(0)
+    view_tasks()
+    save_tasks()
